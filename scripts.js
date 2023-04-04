cf1a23365f3110ca679147bea60c487c8b1d29ed
rafaelanjosjesus <veigarnodraiven@gmail.com>
2023-04-04 11:21:56 -0300

    I finish my simple trafficLight :D

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let traficLight = "red"
+    switch (traficLight) {
+        case "red":
+            console.log("Stop");
+                break;
+
+        case "yellow":
+            console.log("Attention");
+                break;
+
+        case "green":
+            console.log("Go");
+                break; 
+
+        default:
+            console.log("Color not found")         
+}
